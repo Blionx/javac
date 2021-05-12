938dfa3584e333349fe8d5ccb728cec026fa94a4
lpens <luis.pens@mercadolibre.com>
2021-05-12 08:56:37 -0300

    Automatically generated project scaffolding.

---
+++ src/main/java/com/mercadolibre/pens_luis_bootcamp_final/beans/RandomSampleBean.java
@@ -0,0 +1,15 @@
+package com.mercadolibre.pens_luis_bootcamp_final.beans;
+
+import com.mercadolibre.pens_luis_bootcamp_final.dtos.SampleDTO;
+import java.util.Random;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RandomSampleBean {
+	private Random random = new Random();
+
+	public SampleDTO random() {
+		return new SampleDTO(random.nextInt(Integer.MAX_VALUE));
+	}
+}
+
